293bfeceaa6a91ad0123dbeab892571c88ca0708
darshannegi <30504726+darshannegi@users.noreply.github.com>
2019-05-14 06:36:23 +0530

    Create BroadcastReciever.java

---
+++ BroadcastReciever.java
@@ -0,0 +1,16 @@
+myreceiver.java
+
+package com.example.broadcastreciever;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Intent i=new Intent(context,MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
